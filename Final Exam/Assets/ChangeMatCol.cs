59689c7addc27b37a54180f1aafe207324c6d08b
Jaden <jaden.bhakti@ontariotechu.net>
2021-12-17 17:15:12 -0500

    material switcher dll + dirty flag

---
+++ Final Exam/Assets/ChangeMatCol.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMatCol : MonoBehaviour
+{
+    [SerializeField] private Material myMat;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            myMat.color = Color.green;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            myMat.color = Color.green;
+        }
+    }
+
+}
